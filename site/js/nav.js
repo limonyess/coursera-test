e4c0718c7c3e10ba844949fe35836ab835bbd7f0
limonyess <0213791@up.edu.mx>
2021-11-20 17:19:04 -0600

    Test page

--- site/js/nav.js
+++ site/js/nav.js
@@ -7,49 +7,37 @@ class Header extends HTMLElement {
     this.innerHTML = `
       <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-        
-        <a class="navbar-brand">
-          <img src="https://limonyess.github.io/coursera-test/site/images/happy_face.png" alt="" class="logo-nav">
-        </a>
-        <button class="navbar-toggler menu-button" href="homepage.html" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse left" id="navbarNavDropdown">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" href="homepage.html" aria-current="page">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="journal.html">Journal</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="gossip.html">Gossip</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Challenges
-              </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <li><a class="dropdown-item" href="physical.html">Physical</a></li>
-                <li><a class="dropdown-item" href="mental.html">Mental</a></li>
-              </ul>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                User profile
-              </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <li><a class="dropdown-item" href="badges.html">Badges</a></li>
-                <li><a class="dropdown-item" href="progress.html">Progress</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div>
+          <div class="container">
+              
+          <a class="navbar-brand">
+            <img src="https://limonyess.github.io/coursera-test/site/images/happy_face.png" alt="" class="logo-nav">
+          </a>
+          <button class="navbar-toggler menu-button" href="homepage.html" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse left" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="homepage.html" aria-current="page">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="journal.html">Journal</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="challenges.html">Challenges</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="gossip.html">Ranking</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="user.html">Profile</a>
+              </li>
+            </ul>
+          </div>
 
-      </div>
-    </nav>
-      </header>
+        </div>
+      </nav>
+    </header>
     `;
   }
 }
